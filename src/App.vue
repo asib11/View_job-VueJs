d55c92d3af6085f99038bf1e5976354c2b646a2c
Asib <ahmedasib1@gmail.com>
2025-02-10 01:00:26 +0600

    feat #1 : card make and learn about slot

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
   import Navbar from '@/components/Navbar.vue';
   import Hero from '@/components/Hero.vue';
-import HomeCard from './components/HomeCard.vue';
+import HomeCards from './components/HomeCards.vue';
   
 </script>
 
 <template>
   <Navbar />
   <Hero title="This is my title" subtitle="oh my Allah, This is subtitle"/>
-  <HomeCard/>
+  <HomeCards/>
 </template>
 
 <style >
