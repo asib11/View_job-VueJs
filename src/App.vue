ea27c92646311c78b52727adfd2ed7dfe5127c97
Asib <ahmedasib1@gmail.com>
2025-02-09 23:54:50 +0600

    feat #1 : hero card make

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
   import Navbar from '@/components/Navbar.vue';
-  import Hero from '@/components/Hero.vue'
+  import Hero from '@/components/Hero.vue';
+import HomeCard from './components/HomeCard.vue';
+  
 </script>
 
 <template>
   <Navbar />
   <Hero title="This is my title" subtitle="oh my Allah, This is subtitle"/>
+  <HomeCard/>
 </template>
 
 <style >
