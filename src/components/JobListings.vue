81d3c9cf25146ac6a5f24074bcfbf7cae9dc14e4
Asib <ahmedasib1@gmail.com>
2025-02-14 21:05:42 +0600

    feat #2 : dynamic job page and back button

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -12,7 +12,7 @@ const state = reactive({
 });
 onMounted(async() =>{
   try{
-    const response = await axios.get('http://localhost:5000/jobs');
+    const response = await axios.get('/api/jobs');
     state.jobs = response.data
   }catch(err){
     console.log(err)
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeViews from "@/views/HomeViews.vue";
 import JobsViews from "@/views/JobsViews.vue";
 import NotFound from "@/views/NotFound.vue";
+import JobViews from "@/views/JobViews.vue";
 
 const router = createRouter({
     history : createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
             path: '/jobs',
             name : 'jobs',
             component : JobsViews,
+        },
+        {
+            path: '/jobs/:id',
+            name: 'job',
+            component : JobViews
         },
         {
             path: '/:catchAll(.*)',
